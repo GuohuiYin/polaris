d14bb4f129b1975aaf554119d4c3b029457151b2
Yin, Guohui <yingh1983@gmail.com>
2020-11-30 20:01:46 +0800

    added swagger and revised service registration/discovery removed payment svc to simply demo project

--- docker-demo/polaris-app/src/main/java/com/polaris/docker/app/service/impl/PolarisServiceImpl.java
+++ docker-demo/polaris-app/src/main/java/com/polaris/docker/app/service/impl/PolarisServiceImpl.java
@@ -2,9 +2,7 @@ package com.polaris.docker.app.service.impl;
 
 import com.polaris.docker.app.model.PurchaseParameter;
 import com.polaris.docker.app.service.PolarisService;
-import com.polaris.docker.app.service.client.AuthService;
 import com.polaris.docker.app.service.client.OrderService;
-import com.polaris.docker.app.service.client.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +10,14 @@ import org.springframework.stereotype.Service;
 public class PolarisServiceImpl implements PolarisService {
 
     @Autowired
-    AuthService authService;
-
-    @Autowired
     OrderService orderService;
 
-    @Autowired
-    PaymentService paymentService;
-
     public String purchase(PurchaseParameter purchaseParameter) {
-        authService.authenticate(purchaseParameter.getUserId());
-
         String order = orderService.order(purchaseParameter.getProduct(),
                 purchaseParameter.getQuantity(), purchaseParameter.getPrice());
 
-        paymentService.pay(order, purchaseParameter.getPrice());
-
-        String response = "product %s with quantity %s has been purchased for price %s";
+        String response = "product [%s] with quantity [%s] has been ordered for price [%s], order id [%s]";
         return String.format(response, purchaseParameter.getProduct(),
-                purchaseParameter.getQuantity(), purchaseParameter.getPrice());
+                purchaseParameter.getQuantity(), purchaseParameter.getPrice(), order);
     }
 }
